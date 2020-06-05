d02ac0d1f96d9796340b5e25630125063bc82db2
wxaxiaoyao <765485868@qq.com>
2020-06-05 10:23:09 +0800

    update code

---
+++ Mod/GeneralGameServerMod/main.lua
@@ -0,0 +1,43 @@
+
+local GeneralGameServerMod = commonlib.inherit(commonlib.gettable("Mod.ModBase"),commonlib.gettable("Mod.GeneralGameServerMod"));
+
+
+function GeneralGameServerMod:ctor()
+end
+
+-- virtual function get mod name
+
+function GeneralGameServerMod:GetName()
+	return "HelloWorld"
+end
+
+-- virtual function get mod description 
+
+function GeneralGameServerMod:GetDesc()
+	return "HelloWorld is a plugin in paracraft"
+end
+
+function GeneralGameServerMod:init()
+	LOG.std(nil, "info", "HelloWorld", "plugin initialized");
+end
+
+function GeneralGameServerMod:OnLogin()
+end
+-- called when a new world is loaded. 
+
+function GeneralGameServerMod:OnWorldLoad()
+end
+-- called when a world is unloaded. 
+
+function GeneralGameServerMod:OnLeaveWorld()
+end
+
+function GeneralGameServerMod:OnDestroy()
+end
+
+function GeneralGameServerMod:handleKeyEvent(event)
+	if(event.keyname == "DIK_SPACE") then
+		LOG.std(nil, "info", "DemoEntity", "OnWorldLoad");
+		return true;
+	end
+end
