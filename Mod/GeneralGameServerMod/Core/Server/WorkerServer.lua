1a9abb62cf0976fc1fe0dda8200c1e9c48a238e5
wxaxiaoyao <765485868@qq.com>
2020-07-21 16:57:16 +0800

    update code

--- Mod/GeneralGameServerMod/Core/Server/WorkerServer.lua
+++ Mod/GeneralGameServerMod/Core/Server/WorkerServer.lua
@@ -52,7 +52,7 @@ function WorkerServer:Init(server)
         if (success) then
             Log:Info("成功连接控制服务");
             -- 推送服务器信息到控制器
-            self.SendServerInfoTimer:Change(0, 1000 * 60 * 3); -- 每3分钟上报一次 
+            self.SendServerInfoTimer:Change(0, 1000 * 60 * 2); -- 每2分钟上报一次 
         else
             Log:Info("无法连接控制服务");
         end
@@ -73,6 +73,14 @@ function WorkerServer:SendServerInfo()
         outerIp = self.outerIp,                 -- 外网IP
         outerPort = self.outerPort,             -- 外网Port 
     }));
+end
+
+-- 处理通用数据包
+function WorkerServer:handleGeneral(packetGeneral)
+    local action = packetGeneral.action;
+    if (action == "ServerWorldList") then 
+        self.ServerList = packetGeneral.data;
+    end
 end
 
 -- 连接丢失
