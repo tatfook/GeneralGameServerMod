1cd829d3ba258669aa25b15dbe7a2aa88659022c
wxaxiaoyao <765485868@qq.com>
2021-03-11 14:15:08 +0800

    update code

--- Mod/GeneralGameServerMod/UI/Window/MacroWindow.lua
+++ Mod/GeneralGameServerMod/UI/Window/MacroWindow.lua
@@ -10,6 +10,9 @@ NPL.load("Mod/GeneralGameServerMod/App/ui/Core/Window/MacroWindow.lua");
 -------------------------------------------------------
 ]]
 NPL.load("(gl)script/apps/Aries/Creator/Game/Macros/MacroPlayer.lua");
+NPL.load("(gl)script/ide/System/Windows/KeyEvent.lua");
+local KeyEvent = commonlib.gettable("System.Windows.KeyEvent");
+local InputMethodEvent = commonlib.gettable("System.Windows.InputMethodEvent");
 local MacroPlayer = commonlib.gettable("MyCompany.Aries.Game.Tasks.MacroPlayer");
 local Macros = commonlib.gettable("MyCompany.Aries.Game.GameLogic.Macros");
 
@@ -50,6 +53,13 @@ end
 
 local function AddMacro(funcname, params)
     Macros:AddMacro(funcname, params);                   -- 为方便扩展, 参数尽量使用对象
+    if (funcname == "UIWindowKeyBoard") then
+        local macros = Macros.macros;
+        local count = #macros;
+        if (count > 2 and macros[count - 2].name == "UIWindowInputMethod" and macros[count - 1].name == "UIWindowKeyBoardTrigger") then
+            macros[count - 2], macros[count - 1] = macros[count - 1], macros[count - 2];
+        end
+    end
 end
 
 local function GetButtonsState()
@@ -97,6 +107,90 @@ function Macros.UIWindowClickTrigger(params)
     end
 end
 
+-- System.Window's key down event
+function Macros.UIWindowKeyBoard(params)
+    local window = MacroWindow.GetWindow(params.macro_name);
+    if (not window) then return end
+    local event = KeyEvent:init("keyPressEvent");
+    event.keyname, event.shift_pressed, event.alt_pressed, event.ctrl_pressed, event.key_sequence = params.keyname, params.shift_pressed, params.alt_pressed, params.ctrl_pressed, params.key_sequence;
+    window:HandleKeyEvent(event);
+end
+
+function Macros.UIWindowKeyBoardTrigger(params)
+    local window = MacroWindow.GetWindow(params.macro_name);
+    if (not window) then return end
+    
+    local buttons = params.keyname or "";
+	if(params.ctrl_pressed) then buttons = "ctrl+"..buttons end
+	if(params.alt_pressed) then buttons = "alt+"..buttons end
+	if(params.shift_pressed) then buttons = "shift+"..buttons end
+    if (buttons == "") then return end
+
+    -- get final text in editbox
+    local nOffset = 0;
+    local targetText = "";
+    while(true) do
+        nOffset = nOffset + 1;
+        local nextMacro = Macros:PeekNextMacro(nOffset)
+        if(nextMacro and (nextMacro.name == "Idle" or nextMacro.name == "UIWindowKeyBoardTrigger" or nextMacro.name == "UIWindowInputMethod" or nextMacro.name == "UIWindowKeyBoard")) then
+            if(nextMacro.name == "UIWindowKeyBoard") then
+                local text = nextMacro:GetParams()[1].keyname;
+                if(not text or not Macros.IsButtonLetter(text)) then
+                    break;
+                end
+            elseif(nextMacro.name == "UIWindowInputMethod") then
+                local text = nextMacro:GetParams()[1].commit_string;
+                if(text) then
+                    targetText = targetText..text;
+                else
+                    break;
+                end
+            end
+        else
+            break;
+        end
+    end
+    if(targetText and targetText ~= "") then
+        local nOffset = 0;
+        while(true) do
+            nOffset = nOffset - 1;
+            local nextMacro = Macros:PeekNextMacro(nOffset)
+            if(nextMacro and (nextMacro.name == "Idle" or nextMacro.name == "UIWindowKeyBoardTrigger" or nextMacro.name == "UIWindowInputMethod" or nextMacro.name == "UIWindowKeyBoard")) then
+                if(nextMacro.name == "UIWindowKeyBoard") then
+                    local text = nextMacro:GetParams()[1].keyname;
+                    if(not text or not Macros.IsButtonLetter(text)) then
+                        break;
+                    end
+                elseif(nextMacro.name == "UIWindowInputMethod") then
+                    local text = nextMacro:GetParams()[1].commit_string;
+                    if(text) then
+                        targetText = text..targetText;
+                    else
+                        break;
+                    end
+                end
+            else
+                break;
+            end
+        end
+    end
+
+    local callback = {};
+    MacroPlayer.SetKeyPressTrigger(buttons, targetText, function()
+        if(callback.OnFinish) then
+            callback.OnFinish();
+        end
+    end);
+    return callback;
+end
+
+-- System.Window's input method event
+function Macros.UIWindowInputMethod(params)
+	local window = MacroWindow.GetWindow(params.macro_name);
+    if (not window) then return end
+    window:HandleKeyEvent(InputMethodEvent:new():init(params.commit_string));
+end
+
 -- 监听处理事件
 function MacroWindow.HandleEvent(event_type, window)
     local macro_name = window:GetMacroName();
@@ -132,6 +226,32 @@ function MacroWindow.HandleEvent(event_type, window)
             win_offset_y = offset_y, 
             mouse_down_up_distance = mouse_down_up_distance,
         });  -- 为方便扩展, 参数尽量使用对象
+    end
+
+    if (event_type == "onkeydown") then
+        local event = KeyEvent:init("keyPressEvent");
+        if(not event:IsShiftCtrlAltKey()) then
+            local params = {macro_name = macro_name, ctrl_pressed = event.ctrl_pressed, shift_pressed = event.shift_pressed, alt_pressed = event.alt_pressed, keyname = event.keyname, key_sequence = event.key_sequence};
+            if(event:IsKeySequence("Paste")) then
+                local text = ParaMisc.GetTextFromClipboard();
+                if(Macros.lastCopyTextToClipboard ~= text) then
+                    -- tricky: if we are pasting from external apps, we need to save the clipboard content
+                    Macros:AddMacro("SetClipboard", text);
+                    if(Macros.IsInteractiveMode()) then
+                        -- also ignore the Ctrl+V trigger, if pasting from external app
+                        Macros.SetInteractiveMode(false)
+                        AddMacro("UIWindowKeyBoard", params);
+                        Macros.SetInteractiveMode(true)
+                        return
+                    end
+                end
+            end
+            AddMacro("UIWindowKeyBoard", params);
+        end
+    end
+
+    if (event_type == "oninputmethod") then
+        AddMacro("UIWindowInputMethod", {macro_name = macro_name, commit_string = msg});
     end
 
 end
