044126a1f9c599ee33827b602a740bafc979442e
wxaxiaoyao <765485868@qq.com>
2022-04-12 15:57:45 +0800

    update code

--- Mod/GeneralGameServerMod/Command/PluginManager/PluginManager.lua
+++ Mod/GeneralGameServerMod/Command/PluginManager/PluginManager.lua
@@ -18,44 +18,61 @@ local Page = NPL.load("Mod/GeneralGameServerMod/UI/Page.lua");
 
 local PluginManager =  commonlib.inherit(commonlib.gettable("System.Core.ToolBase"), NPL.export());
 
+-- this can be modified via https://keepwork.com/official/paracraft/config/PluginManager 
+local ds_url = "https://api.keepwork.com/core/v0/repos/official%2Fparacraft/files/official%2Fparacraft%2Fconfig%2FPluginManager.md";
+
 PluginManager.PluginList = {
-    {
-        name = "MineCraft 地图导入导出插件",
-        desc = "支持导入 MineCraft 世界, 以及 Schematics 地图文件",
-        url = "https://ghproxy.com/https://github.com/tatfook/mc/releases/download/v1.0.0/MCImporter.zip",
-        key = "MCImporter.zip",
-    },
+    -- {
+    --     name = "MineCraft 地图导入导出插件",
+    --     desc = "支持导入 MineCraft 世界, 以及 Schematics 地图文件",
+    --     url = "https://ghproxy.com/https://github.com/tatfook/mc/releases/download/v1.0.0/MCImporter.zip",
+    --     key = "MCImporter.zip",
+    -- },
 }
 
 function PluginManager:GetLoader()
 	return ModManager:GetLoader();
 end
 
+function PluginManager:LoadDataSource()
+    if (self.loaded) then return end;
+    self.loaded = true;
+    local plugins = {};
+    System.os.GetUrl(ds_url, function(status_code, msg, data)
+        if (status_code ~= 200) then return end 
+
+        local lines = commonlib.split(data, "\n");
+        for _, line in ipairs(lines) do
+            local url = string.match(line, "http[s]?://[^#]+");
+            local name = string.match(line, "#name=([^#]+)");
+            local desc = string.match(line, "#desc=([^#]+)");
+            local key = string.match(line, "#key=([^#]+)");
+            local project_url = string.match(line, "#project_url=([^#]+)");
+            if (url and key and name) then
+                -- print(url, name, key, desc)
+                plugins[key] = {url = url, name = name, desc = desc or "", key = key, project_url = project_url};
+            end
+        end
+
+        for _, plugin in pairs(plugins) do
+            plugin.path = CommonLib.ToCanonicalFilePath(CommonLib.GetRootDirectory() .. "/Mod/" .. plugin.key);
+            if (not string.match(plugin.path, "%.zip$")) then plugin.path = plugin.path .. ".zip" end
+            plugin.state = CommonLib.IsExistFile(plugin.path) and 1 or 0;
+    
+            table.insert(self.PluginList, plugin);
+        end
+
+        if (self.__ui__) then self.__ui__:GetG():RefreshWindow() end 
+    end);
+end
+
 function PluginManager:Init()
-    for _, plugin in ipairs(self.PluginList) do
-        plugin.path = CommonLib.ToCanonicalFilePath(CommonLib.GetRootDirectory() .. "/Mod/" .. plugin.key);
-        if (not string.match(plugin.path, "%.zip$")) then plugin.path = plugin.path .. ".zip" end
-        plugin.state = CommonLib.IsExistFile(plugin.path) and 1 or 0;
-    end
-
-    -- local xmlRoot = ParaXML.LuaXML_ParseFile(CommonLib.ToCanonicalFilePath(CommonLib.GetRootDirectory() .. "/Mod/ModsConfig.xml"));
-    -- local mods = commonlib.XPath.selectNodes(xmlRoot, "/mods/mod") or {};
-    -- for _, mod in ipairs(mods) do
-    --     local modname = mod.attr and mod.attr.name;
-    --     for _, plugin in ipairs(self.PluginList) do
-    --         if (plugin.key == modname) then
-    --             if (type(mod[0]) == "table" and mod[0].name == "world" and type(mod[0].attr) == "table" and mod[0].attr.checked) then
-    --                 plugin.state = 1;
-    --             end
-    --         end
-    --     end
-    -- end
-
-    return true;
+    return self;
 end
 
 function PluginManager:Show()
     self:Close();
+    self:LoadDataSource();
     self.__ui__ = Page.Show({
         PluginList = self.PluginList,
         ClickInstallPlugin = function(plugin)
@@ -63,8 +80,8 @@ function PluginManager:Show()
         end
     }, {
         url = "Mod/GeneralGameServerMod/Command/PluginManager/PluginManager.html",
-        width = 750,
-        height = 540,
+        width = 647,
+        height = 437,
         draggable = false,
         OnClose = function()
             self.__ui__ = nil;
