89c2aaef4f9df2ad12319e18016a3f5165a3fe98
xiaoyao <765485868@qq.com>
2022-07-26 10:54:00 +0800

    update code

--- Mod/GeneralGameServerMod/UI/Vue/Page/User/User.lua
+++ Mod/GeneralGameServerMod/UI/Vue/Page/User/User.lua
@@ -12,7 +12,7 @@ local SelfProjectList = {};
 local AuthUser = KeepWorkItemManager.GetProfile();
 local player = GameLogic.GetPlayerController():GetPlayer();
 local GlobalScope = GetGlobalScope();
-local PageSize = 10;
+local PageSize = 400;
 
 -- 组件全局变量初始化
 GlobalScope:Set("AuthUsername", AuthUser.username);
