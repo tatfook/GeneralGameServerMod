895960ea4692d8b18c3fbfbd0167eae4aca3e6b7
xiaoyao <765485868@qq.com>
2022-11-21 10:42:14 +0800

    fix blockly mouse wheel direction

--- Mod/GeneralGameServerMod/UI/Blockly/ToolBox.lua
+++ Mod/GeneralGameServerMod/UI/Blockly/ToolBox.lua
@@ -246,9 +246,13 @@ function ToolBox:OnMouseUp(event)
 end
 
 function ToolBox:OnMouseWheel(event)
-    local delta = event:GetDelta();             -- 1 向上滚动  -1 向下滚动
+    local delta = event:GetDelta();                -- 1 向上滚动  -1 向下滚动
     local dist, offset = 16, 5;                 -- 滚动距离为5 * Const.DefaultUnitSize  
-
+    
+    if (GameLogic.options:HasTouchDevice()) then
+        delta = event:GetBlocklyToolBoxDelta();
+    end
+    
     if (#self.blocks == 0) then return end
     local scale = self:GetScale();
     local heightUnitCount = math.floor(self.heightUnitCount / scale);
